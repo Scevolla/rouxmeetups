d0836a452de1c2a1194eae543dedb9a5ed30da75
Scevolla <moyaemailpochta@gmail.com>
2017-07-18 00:41:50 +0600

    Added ejs and bootstrap

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.send(`
-      <link rel="stylesheet" type="text/css" href="css/style.css">
-      <h1>Welcome</h1>
-      <img src="/images/misc/background.jpg" alt="background" style="height: 300px;">
-      <p>Roux Academy Meetups put together artists from all walks of life</p>
-  `);
+  res.render('index_view');
 });
 
 module.exports = router;
